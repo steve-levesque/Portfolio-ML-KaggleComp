044e955f855dbf9a5d80864c4db54c48c2d4208c
Steve Lévesque <42849270+steve-levesque@users.noreply.github.com>
2022-01-09 14:55:02 -0500

    [Add] Template Run File
    - Added template to see how to structure a run file for a problem to solve.

---
+++ run_template.py
@@ -0,0 +1,38 @@
+# All your imports necessary.
+import numpy as np
+
+# ======================================================================================================================
+# Data augmentation functions.
+
+# ...
+# ======================================================================================================================
+
+
+# ======================================================================================================================
+# Models for solver algorithms or others.
+
+# ...
+# ======================================================================================================================
+
+if __name__ == '__main__':
+    print("Run run_template example struct template.")
+    print("See run_comp1, run_comp2 and run_mnist for more examples...")
+
+    # Declare and use one or multiple solvers.
+
+    """
+    solver = models.solver.KaggleCompSolver(
+        comp_name='mnist',
+        train_label_pos=0,
+        comp_method_type='supervised-classification',
+        comp_task_type='image',
+        comp_algo='cnn'
+    )
+    """
+
+    # solver.info(logs=True)
+
+    # Use functions from above for data augmentation, get and set the data.
+    # The data is compiled by the solver, but only basic operations are made on it (i.e. simple splits, parses, etc.)
+
+    # solver.solve()
